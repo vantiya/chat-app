eb6510e0d1c1548a34a5e3d1156f7c8ac2c5b466
vantiya <parvezvantiya@gmail.com>
2021-01-04 15:13:15 +0530

    Added Static Message by click floating Add button to Firebase DB.

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -6,24 +6,35 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('Hey there!'),
-        ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+          .collection('chats/LjL8bfm2wng8lPZiwm6m/messages')
+          .snapshots(),
+        builder: (ctx, streamSnapshot) {
+          if( streamSnapshot.connectionState == ConnectionState.waiting ) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (ctx, index) =>
+                Container(
+                  padding: EdgeInsets.all(8),
+                  child: Text( documents[index]['text'] ),
+                ),
+          );
+        }
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/LjL8bfm2wng8lPZiwm6m/messages')
-              .snapshots()
-              .listen((data) {
-                data.docs.forEach((document) {
-                  print( document['text'] );
-                });
-            print(data.docs.first);
+          FirebaseFirestore.
+          instance.
+          collection('chats/LjL8bfm2wng8lPZiwm6m/messages').
+          add({
+            'text': 'This is added by clicking plus button.',
           });
         },
       ),
